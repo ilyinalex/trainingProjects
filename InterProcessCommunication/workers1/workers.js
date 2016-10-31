eb1c47e5f18af2e7d1d290b3914a07f3737e6480
ilyinalex <airbam@yandex.ru>
2016-10-31 12:59:52 +0200

    Create workers.js

---
+++ InterProcessCommunication/workers1/workers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = () => {
+  console.log('Hello from worker ' + process.pid + ' ' + api.cluster.worker.id);
+
+  process.on('message', (message) => {
+    console.log(
+      'message to worker ' + process.pid +
+      ' from master: ' + JSON.stringify(message)
+    );
+    process.send({
+      result: message.task.map((item) => item * 2),
+      id:api.cluster.worker.id
+    });
+   
+    
+  });
+
+};
