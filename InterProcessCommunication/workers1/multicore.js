f9b67508bb9df962e32f3af1deca42f99f4e44c0
ilyinalex <airbam@yandex.ru>
2016-10-31 12:58:54 +0200

    Create multicore.js

---
+++ InterProcessCommunication/workers1/multicore.js
@@ -0,0 +1,12 @@
+'use strict';
+
+global.api = {};
+api.cluster = require('cluster');
+api.os = require('os');
+
+global.application = {};
+application.master = require('./master.js');
+application.worker = require('./worker.js');
+
+if (api.cluster.isMaster) application.master();
+else application.worker();
