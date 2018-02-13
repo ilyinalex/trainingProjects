41171d49724bcd52661add710fbc2f1a7556d37c
Alexey Ilin <alexeyilin@localhost.localdomain>
2018-02-13 17:17:03 +0200

    DimaCommit

--- InterProcessCommunication/workers1/multicore.js
+++ InterProcessCommunication/workers1/multicore.js
@@ -7,6 +7,6 @@ api.os = require('os');
 global.application = {};
 application.master = require('./master.js');
 application.worker = require('./worker.js');
-
+//Hello, world!!!
 if (api.cluster.isMaster) application.master();
 else application.worker();
